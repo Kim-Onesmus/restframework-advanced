0faa65e8214bcc28783b39e3af2a794f4e5adb44
Kim-Onesmus <kimones1122@gmail.com>
2024-04-14 22:06:41 +0300

    changes

--- py_client/basic.py
+++ py_client/basic.py
@@ -5,9 +5,10 @@ import requests
 endpoint = "http://127.0.0.1:8000/"
 
 
-get_response = requests.get(endpoint, json="Hello world")
-print(get_response.text)
+get_response = requests.get(endpoint, params={'abc':123}, json="Hello world")
+# print(get_response.text)
+# print(get_response.status_code)
 
 
 print(get_response.json()) 
-print(get_response.status_code)
+# print(get_response.status_code)
