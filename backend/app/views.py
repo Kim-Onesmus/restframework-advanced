b06abab522bb9dfdfb48a4ff2b81ae39f5d48f88
Kim-Onesmus <kimones1122@gmail.com>
2024-04-14 22:37:16 +0300

    changes

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django .http import JsonResponse
+import json
 
 
 def api_home(request, *args, **kwargs):
-
-    body = request.body
-    print(body)
-    return JsonResponse({'message': 'Hi there'})
+    
+    return JsonResponse(data)
