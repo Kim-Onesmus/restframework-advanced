49f8904e60deb20e31e608a2aa7ba3d57a247a58
Kim-Onesmus <kimones1122@gmail.com>
2024-04-14 21:27:02 +0300

    changes

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,4 +1,12 @@
 import requests
 
-endpoint = "https://httpbin.org/"
-endpoint = "https://httpbin.org/status/200/"
+# endpoint = "https://httpbin.org/status/200/"
+endpoint = "https://httpbin.org/anything"
+
+
+get_response = requests.get(endpoint, json="Hello world")
+print(get_response.text)
+
+
+print(get_response.json()) 
+print(get_response.status_code)
